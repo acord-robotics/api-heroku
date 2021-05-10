f17bb8c3546d5f8086cdc13b3fe640752fea49b1
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-05-10 12:34:41 +0800

    DSP-27 New build_database for notes API

--- models.py
+++ models.py
@@ -7,6 +7,22 @@ class Person(db.Model):
     lname = db.Column(db.String(32), index=True)
     fname = db.Column(db.String(32))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    notes = db.relationship(
+        'Note',
+        backref='person',
+        cascade='all, delete, delete-orphan',
+        single_parent=True,
+        order_by='desc(Note.timestamp)'
+        )
+
+class Note(db.Model):
+    __tablename__ = 'note'
+    note_id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.person_id'))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
 class PersonSchema(ma.ModelSchema):
     class Meta:
