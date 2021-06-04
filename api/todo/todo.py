6b68e7dcad4b2397273e60c429a483603d314196
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-06-04 21:49:07 +0800

    ✅ Add simple to-do api functionality to react app /api/ GSCA-4

---
+++ api/todo/todo.py
@@ -0,0 +1,12 @@
+def get_tasks():
+    pass
+def describe_task(task_id):
+    pass
+def add_task(summary, description=""):
+    pass
+def task_done(task_id):
+    pass
+def update_task(task_id, summary, description):
+    pass
+def _url(path):
+    return 'https://todo.example.com' + path
